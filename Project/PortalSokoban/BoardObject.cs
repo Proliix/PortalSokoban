8f6c8562299943dfb08f8882dca8f6c174ccc92a
Proliix <lucas.andreasson@hotmail.com>
2023-02-09 13:05:48 +0100

    BoardObjectBase

---
+++ Project/PortalSokoban/BoardObject.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalSokoban
+{
+    public class BoardObject
+    {
+        Vector2 drawPos;
+        int xPos;
+        int YPos;
+        Texture2D sprite;
+
+        BoardObject(int xPos, int yPos)
+        {
+            this.YPos = yPos;
+            this.xPos = xPos;
+            drawPos = new Vector2(xPos * Board.CELL_WIDTH, yPos * Board.CELL_HEIGHT);
+        }
+    }
+}
