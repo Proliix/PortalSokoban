e16d3899b9f896e6389c02f2d957b98b5cdfcea9
Proliix <lucas.andreasson@hotmail.com>
2023-02-09 12:00:22 +0100

    rename

---
+++ Project/PortalSokoban/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortalSokoban
+{
+    internal class Board
+    {
+        int WIDTH = 32;
+        int HEIGHT = 32;
+    }
+}
--- Project/PortalSokoban/PortalSokoban.cs
+++ Project/PortalSokoban/PortalSokoban.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace PortalSokoban
 {
-    public class Game1 : Game
+    public class PortalSokoban : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public PortalSokoban()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
