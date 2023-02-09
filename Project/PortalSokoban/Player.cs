93e00570db9eb79a33ecf1d3fe6aa0039d0fc060
sweviceroy <sweviceroy@gmail.com>
2023-02-09 15:48:14 +0100

    Added player texture for direction down

---
+++ Project/PortalSokoban/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Player
+{
+	private bool isAlive;
+	private int direction;
+	private Texture2D currentSprite;
+
+	public Player()
+	{
+
+	}
+}
---
+++ Project/PortalSokoban/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing.Text;
+
+public class Portal
+{
+	private bool portal1placed;
+	private bool portal2placed;
+
+	private (int,int) portal1Pos;
+	private (int,int) portal2Pos;
+	public Portal()
+	{
+		portal1placed= false;	
+		portal2placed= false;
+
+	}
+	public (int, int) getPortal1Pos()
+	{
+		return portal1Pos;
+	}
+    public (int, int) getPortal2Pos()
+    {
+        return portal1Pos;
+    }
+	public void setPortal1Placed(bool _bool)
+	{
+		portal1placed= _bool;
+	}
+    
+	
+}
