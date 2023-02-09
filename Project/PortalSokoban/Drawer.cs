b0bc1f36b0100d3eacd3b68fa7aad2bda8785da4
sweviceroy <sweviceroy@gmail.com>
2023-02-09 14:37:57 +0100

    updated board class with texture2d for each tile (ground and wall)
    Dont pay atention to drawer class, will probably rekt it later

---
+++ Project/PortalSokoban/Drawer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Drawer
+{
+    // |||||||||||||||||||||||||||||||||||||||||||||||||||
+    // || TILES 
+    // |||||||||||||||||||||||||||||||||||||||||||||||||||
+    /// </summary>
+    
+    // GROUNDTILES
+    #region
+    private const int TotalGroundTiles = 5;
+
+    private const int GroundTile0 = 0;
+    private const int GroundTile1 = 1;
+    private const int GroundTile2 = 2;
+    private const int GroundTile3 = 3;
+    private const int GroundTile4 = 4;
+    #endregion
+    // WALLTILES
+    #region
+    private const int TotalWallTiles = 5;
+
+    private const int WallTile0 = 5;
+    private const int WallTile1 = 6;
+    private const int WallTile2 = 7;
+    private const int WallTile3 = 8;
+    private const int WallTile4 = 9;
+    #endregion
+    // OTHER 
+    private const int BOX = 10;
+    private const int KNAPP = 11;
+    private const int DOORCLOSE = 12;
+    private const int DOOROPEN = 13;
+    private const int PLAYER = 14;
+    private const int PORTAL1 = 15;
+    private const int PORTAL2 = 16;
+    
+
+    
+}
